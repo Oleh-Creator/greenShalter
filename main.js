99823b748852ba9c99de27825b90b0563720b148
Oleh <105147109+Oleh-Creator@users.noreply.github.com>
2023-03-27 01:07:01 +0300

    not bad

---
+++ main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper-1", {
+    effect: "cards",
+    grabCursor: true,
+  });
