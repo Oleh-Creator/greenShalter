d88aa66753bdc5d6ef0297bfe8ec0785ec55a8c3
Oleh <105147109+Oleh-Creator@users.noreply.github.com>
2023-03-30 23:47:22 +0300

    Payment done

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  var swiper = new Swiper(".mySwiper-1", {
-    effect: "cards",
-    grabCursor: true,
-  });
+var swiper = new Swiper(".mySwiper-1", {
+  effect: "cards",
+  grabCursor: true,
+});
+$('.suportBut').click(function(){
+  $('.payBox').css('display','flex');
+});
+$('.payExit').click(function(){
+  $('.payBox').css('display','none');
+});
+$('.cardBut').click(function(){
+  $('.payCardBox').css('display','flex');
+  $('.payBox').css('display','none');
+});
+$('.payCardExit').click(function(){
+  $('.payCardBox').css('display','none');
+});
+
+$('.CardDogBut').click(function(){
+  $('.payCardBox').css('display','none');
+});
